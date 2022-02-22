cb6b9d5dc84d04e10c1e01060738a16834150a02
SerhVo <svorotnoy@gmail.com>
2022-02-23 00:11:47 +0200

    finally 23-02

--- js/script.js
+++ js/script.js
@@ -9,36 +9,73 @@ burger.addEventListener('click', () => {
 
 navigationClose.addEventListener('click', () => {
     navigation.classList.remove('navigation_active');  
-
 });
 
 
 //Музыка
-const mute = document.querySelector('.mute__checkbox');
+try {
+    const mute = document.querySelector('.mute__checkbox');
+const audio = new Audio('audio/waterTower.mp3')
+
+const checkMute = () => {
+    if (mute.checked) {
+        audio.play();
+    } else {
+        audio.pause();
+    }
+};
+
+checkMute();
+
+setTimeout(checkMute, 5000)
+
+mute.addEventListener('change', checkMute)
+} catch {
+    console.log('На этой странице нет музыки')
+}   
 
 
 
 try {
-const sliderThumbs = new Swiper('.slider-thumbs', {
-    loop: true,
-    spaceBetween: 20,
-    slidesPerView: 3,
-    centeredSlides: true,
-    loopedSlides: 4,
-});
+    const sliderThumbs = new Swiper('.slider-thumbs', {
+        loop: true,
+        spaceBetween: 20,
+        slidesPerView: 4,
+        centeredSlides: true,
+        loopedSlides: 4,
+    });
 
-sliderThumbs.on('click', (swiper) => {
-    swiper.slideTo(swiper.clickedIndex)
-})
+    sliderThumbs.on('click', (swiper) => {
+        swiper.slideTo(swiper.clickedIndex)
+    })
 
-const sliderMain = new Swiper('.slider-main', {
-    loop: true,
-    spaceBetween:10,
-    loopedSlides: 4,
-});
+    const sliderMain = new Swiper('.slider-main', {
+        loop: true,
+        spaceBetween:10,
+        loopedSlides: 4,
+    });
 
-sliderThumbs.controller.control = sliderMain;
-sliderMain.controller.control = sliderThumbs;
+    sliderThumbs.controller.control = sliderMain;
+    sliderMain.controller.control = sliderThumbs;
+
+    const video = document.querySelectorAll('video');
+
+    sliderMain.on('slideChange', () => {
+        for (let i = 0; i < video.length; i += 1) {
+            video[i].pause();
+        }
+    });
+
+    const pagination = document.querySelector('.pagination');
+    const paginationButton = document.querySelector('.pagination__arrow');
+
+    paginationButton.addEventListener('click', () => {
+        pagination.classList.toggle('pagination_active')
+    })
+
+    pagination
+pagination__arrow
+
 } catch {
-    console.log('На этой странице нет слайдера')
+        console.log('На этой странице нет слайдера')
 }
